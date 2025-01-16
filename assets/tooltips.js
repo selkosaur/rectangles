bfed364274d1eb388d0718747aa5d8710965beaa
so <jwthecowicorn@gmail.com>
2025-01-16 13:29:03 -0500

    singletons

--- assets/tooltips.js
+++ assets/tooltips.js
@@ -1,7 +1,37 @@
-import tippy, * as tiptools from "./tippyes.js";
+import tippy, { createSingleton} from "./tippyes.js";
 import tipcss from "./tippydefault.css" with { type: "css" };
-document.adoptedStyleSheets = [tipcss];
+document.adoptedStyleSheets = [...document.adoptedStyleSheets,tipcss];
 
+/**
+ * @typedef {Object} TippyInstance
+ * @property {Function} clearDelayTimeouts - Function to clear delay timeouts.
+ * @property {Function} destroy - Function to destroy the tooltip instance.
+ * @property {Function} disable - Function to disable the tooltip.
+ * @property {Function} enable - Function to enable the tooltip.
+ * @property {Function} hide - Function to hide the tooltip.
+ * @property {Function} hideWithInteractivity - Function to hide the tooltip with interactivity.
+ * @property {number} id - Unique identifier for the tooltip instance.
+ * @property {Array} plugins - Array of plugins associated with the tooltip.
+ * @property {HTMLElement} popper - Popper element associated with the tooltip.
+ * @property {null} popperInstance - Instance of the Popper.js instance (currently null).
+ * @property {Object} props - Properties of the tooltip.
+ * @property {HTMLElement} reference - Reference element for the tooltip.
+ * @property {Function} setContent - Function to set the content of the tooltip.
+ * @property {Function} setProps - Function to set the properties of the tooltip.
+ * @property {Function} show - Function to show the tooltip.
+ * @property {Object} state - State of the tooltip.
+ * @property {boolean} state.isEnabled - Indicates if the tooltip is enabled.
+ * @property {boolean} state.isVisible - Indicates if the tooltip is visible.
+ * @property {boolean} state.isDestroyed - Indicates if the tooltip is destroyed.
+ * @property {boolean} state.isMounted - Indicates if the tooltip is mounted.
+ * @property {boolean} state.isShown - Indicates if the tooltip is shown.
+ * @property {Function} unmount - Function to unmount the tooltip.
+ */
+
+
+/**
+ * @type {Array<TippyInstance>}
+ */
 const tips = tippy("[title]", {
   content: (reference) => {
     let tip = reference.getAttribute("title");
@@ -10,5 +40,40 @@ const tips = tippy("[title]", {
   },
   placement: "bottom",
 });
+
+
+/**
+ * @type {Map<string, Array<TippyInstance>>}
+ */
+const singletonInstancesMap = new Map();
+
+tips.forEach(tip => {
+  const { reference } = tip;
+  const dataSingletonId = reference.getAttribute("data-singleton");
+  if (!dataSingletonId) return;
+  if (singletonInstancesMap.has(dataSingletonId)) {
+    const arr = singletonInstancesMap.get(dataSingletonId);
+    arr.push(tip);
+  }
+  else {
+    singletonInstancesMap.set(dataSingletonId, [tip]);
+  }
+});
+
+
+const singletonsRef = new Map();
+
+const initSingletons = () => {
+  singletonInstancesMap.forEach((instances, id) => {
+    const inst0 = instances[0];
+
+    const singleton = createSingleton(instances, {
+      moveTransition: 'transform 0.2s ease-out',
+      placement:inst0.props.placement
+    });
+    singletonsRef.set(id, singleton);
+  })
+}
+initSingletons();
 
 export default tips;
