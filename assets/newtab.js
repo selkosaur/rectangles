25b2bfeb0d765eb960c83975e98a4e1ae5f713d2
so <jwthecowicorn@gmail.com>
2025-01-20 13:37:59 -0500

    export isnewtab, only run updatepage initially

--- assets/newtab.js
+++ assets/newtab.js
@@ -1,4 +1,5 @@
-let isNewTab = window.location.href.indexOf("chrome-extension://") > -1;
+export const isNewTab =
+  window.location.href.indexOf("chrome-extension://") > -1;
 
 const changeTitle = () => {
   document.querySelector("title").innerText = "New Tab";
@@ -8,12 +9,20 @@ const changeIcon = () => {
   document.querySelector("link[rel='icon']").href = "./assets/newtab.png";
 };
 
-const updatePage = (() => {
+const updatePage = () => {
   if (!isNewTab) {
     return;
   }
   changeTitle();
   changeIcon();
-})();
+};
+
+let initialChange = false;
+
+!initialChange &&
+  (() => {
+    updatePage();
+    initialChange = true;
+  })();
 
 export default updatePage;
